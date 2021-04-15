7a373c888ef0ec8fc70a5fc580d3ee5140778aa1
WDRK <lllrrrlrssr@gmail.com>
2021-04-15 15:03:44 +0900

    각종 타입들의 초기화 방법

---
+++ src/defineTypes.ts
@@ -0,0 +1,10 @@
+let myBoolean: boolean = true;
+let myNumber: number = 10;
+let myString: string = 'Hello';
+let myUndefined: undefined;
+let myNull: null = null;
+let myObject: { name: string } = { name: 'happy' };
+let myObject2: number[] = [1, 2, 3];
+let myFunction: (a: number) => number = function (a) {
+  return a;
+};
