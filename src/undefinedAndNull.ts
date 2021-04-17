bac1994ef5f979d4f227ee1ee6b707376f9e8f40
WDRK <lllrrrlrssr@gmail.com>
2021-04-17 12:40:15 +0900

    undefined와 null의 차이점 확인

---
+++ src/undefinedAndNull.ts
@@ -0,0 +1,12 @@
+let testUndefined: any;
+console.log(testUndefined, typeof testUndefined);
+
+const testUndefined2: undefined = undefined;
+console.log(testUndefined2, typeof testUndefined2);
+
+const testNull: null = null;
+console.log(testNull, typeof testNull);
+
+console.log(null === undefined);
+console.log(null == undefined);
+console.log(undefined == undefined);
